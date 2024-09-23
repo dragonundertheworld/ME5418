70f10bbf1afdf33b37362ca73235ff8f4c94aefc
dragonundertheworld <1300464438@qq.com>
2024-09-23 10:57:11 +0800

    Add MapBuilder module for generating random maps and saving them

---
+++ MapBuilder.py
@@ -0,0 +1,39 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+
+def MapBuilder(row, col):
+    map = np.ones((row, col), dtype=int)
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    def dfs(x, y):
+        map[x, y] = 0
+        random.shuffle(directions)
+        for dx, dy in directions:
+            nx, ny = x + 2 * dx, y + 2 * dy
+            if 1 <= nx < row - 1 and 1 <= ny < col - 1 and map[nx, ny] == 1:
+                map[x + dx, y + dy] = 0  
+                dfs(nx, ny)
+    
+    dfs(1, 1)
+    return map
+
+def save_amd_show_map(map):
+    # save map files
+    timestamp = time.strftime("%Y-%m-%d_%H-%M-%S")
+    file_name = f"map_{timestamp}.{'txt'}"  
+    file_path = f'./test_map/{file_name}'
+    with open(file_path, 'w') as file:
+        file.write(np.array2string(map))
+
+    # show map
+    plt.imshow(map, cmap='gray', interpolation='none')
+    plt.colorbar()  
+    plt.title(file_name)  
+    plt.show()
+
+
+# matrix = MapBuilder(30, 30)
+# save_amd_show_map(matrix)
